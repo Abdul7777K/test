44c4e5527fa55a7f067c3c5dd4fa3175c69871d0
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2023-04-01 11:29:27 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
-git clone https://github.com/Abdul7777K/test.git
+git clone https://github.com/Abdul7777K/test
 cd test
 rm -rf .git
 
