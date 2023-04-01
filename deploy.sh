5b395afdfa19d3391bc0ae75e96fc48ae9db202e
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2023-04-01 10:45:30 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Assign the environment variables passed as arguments to variables
+AZUREPAT=$1
+AZUSERNAME=$2
+AZUSER_EMAIL=$3
+AZORG=$4
+
+# Clone the source repository and remove the Git metadata
+git clone https://github.com/purushothamreddyaccionlabs/Angular_DotNET_Projects.git
+cd testAzureDevops
+rm -rf .git
+
+# Clone the target repository using Azure DevOps authentication
+GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Abdul/_git/syncWithgitEshopCommerce" 
+git clone $GIT_CMD_REPOSITORY
+
+# Copy the source files to the target repository
+cp -r testAzureDevops/* TestGitSync/
+
+cd TestGitSync
+
+# Set the Git user name and email
+git config --global user.email "$AZUSER_EMAIL"
+git config --global user.name "$AZUSERNAME"
+
+# Commit the changes and push to the target repository
+git add .
+git commit -m "sync from git to azure"
+git push
+
