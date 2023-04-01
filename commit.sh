149abcbd0ace73ed5c937e1b753214a87decf68d
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2023-04-01 11:30:33 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -7,18 +7,4 @@ cd test
 rm -rf .git
 
 cd ..
-
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Abdul/_git/syncWithgitEshopCommerce" 
-git clone $GIT_CMD_REPOSITORY
-
-cp -r test/* syncWithgitEshopCommerce/
-
-cd syncWithgitEshopCommerce
-
-git config --global user.email "$AZUSER_EMAIL"
-git config --global user.name "$AZUSERNAME"
-
-git add .
-git commit -m "sync from git to azure"
-
-git push
+ls
