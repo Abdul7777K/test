7fa0bbc3eebb9cfe5b81f6754d1808c60c1c0c10
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2023-04-01 11:51:06 +0530

    Create pwoer.ps1

---
+++ pwoer.ps1
@@ -0,0 +1,25 @@
+$AZUREPAT = $env:AZUREPAT
+$AZUSERNAME = $env:AZUSERNAME
+$AZUSER_EMAIL = $env:AZUSER_EMAIL
+$AZORG = $env:AZORG
+
+git clone https://github.com/Abdul7777K/test.git
+cd test
+Remove-Item -Recurse .git
+
+cd ..
+
+$GIT_CMD_REPOSITORY = "https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Abdul/_git/syncWithgitEshopCommerce"
+git clone $GIT_CMD_REPOSITORY
+
+Copy-Item -Recurse test/* syncWithgitEshopCommerce/
+
+cd syncWithgitEshopCommerce
+
+git config --global user.email "$AZUSER_EMAIL"
+git config --global user.name "$AZUSERNAME"
+
+git add .
+git commit -m "sync from git to azure"
+
+git push
